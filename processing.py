bf3c95a8866c66de65e8ae0dacaae426f6e33c6e
Barbara Morawska <210279@edu.p.lodz.pl>
2020-04-14 01:57:20 +0200

    spektrogram

--- processing.py
+++ processing.py
@@ -1,7 +1,7 @@
 from typing import List
-
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def preEmphasis(signal: list, alpha: float) -> np.array:
@@ -27,3 +27,36 @@ def calc_fft(y, rate):
     freq = np.fft.rfftfreq(n, d=(1 / rate))
     Y = abs(np.fft.rfft(y / n))
     return Y, freq
+
+
+def spectrogram(x, rate, windowsize=512, off=0.01, draw=True, title="Spectrogram"):
+    """
+    example
+    x, rate = librosa.load(file, sr=16000)
+    spectrogram(x, rate)
+    """
+    window_size = windowsize
+    offset = off  # 10 ms
+    step = int(rate * offset)
+    nfft = window_size
+    hamming = np.hamming(window_size)
+
+    step_range = range(step, len(x) - window_size, step)
+    F = np.zeros((len(step_range), nfft // 2))
+
+    for i, n in enumerate(step_range):
+        window = x[n: n + window_size] * hamming
+        z = np.fft.fft(window)
+        F[i, :] = np.abs(z[:nfft // 2])
+
+    freq = np.arange(0, window_size / 2) * rate / window_size
+    time = np.arange(0, len(step_range)) * offset
+
+    if draw:
+        plt.pcolormesh(time, freq, F.T)
+        plt.title(title)
+        plt.show()
+
+    return time, freq, F.T
+
+
